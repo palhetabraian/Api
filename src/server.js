ebce4631c7ad66d1213cb0d844cccc5172f120b2
Braian <braiannickolas12@gmail.com>
2024-10-28 13:42:17 -0300

    Aula 313
    Aprendendo e usando Route Params

--- src/server.js
+++ src/server.js
@@ -2,5 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params;
+  response.send(`
+    Mensagem ID: ${id}. 
+    Para o usuario: ${user}.
+    `);
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
