09005ecc601c4335994463dd3f2feb09db9721fe
Braian <braiannickolas12@gmail.com>
2024-11-28 19:20:11 -0300

    body params
    Aula 318

--- src/server.js
+++ src/server.js
@@ -1,19 +1,11 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
-  response.send(`
-    Mensagem ID: ${id}. 
-    Para o usuario: ${user}.
-    `);
-});
-
-app.get("/users", (request, response) => {
-  const { page, limit } = request.query;
-
-  response.send(`Pagina ${page}. Mostrar:${limit}`);
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body;
+  response.json({ name, email, password });
 });
 
 const PORT = 3333;
