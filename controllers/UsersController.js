4ff51532fca629e7a7830648e3d69604731cad75
Braian <braiannickolas12@gmail.com>
2025-01-07 19:05:22 -0300

    Aula 320
    Controllers

---
+++ controllers/UsersController.js
@@ -0,0 +1,10 @@
+class UsersController {}
+/**
+ * index - GET para listar varios registros.
+ * show - GET para exibir um registro especefico.
+ * create - POST para criar um registro.
+ * update - PUT para atualizar um registro.
+ * delete - DELETE para remover um registro.
+ */
+
+module.exports = UsersController;
