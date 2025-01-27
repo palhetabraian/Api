ab4e42a4fd2e693687a2f1a5c38a16defec507b3
Braian <braiannickolas12@gmail.com>
2025-01-27 20:08:14 -0300

    Utilizando Middleware
    Aula324

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,8 +4,18 @@ const UsersController = require("../controllers/UsersController");
 
 const userRoutes = Router();
 
+function myMiddleware(request, response, next) {
+  console.log("voce passou pelo middleware");
+
+  if (!request.body.isAdmin) {
+    return response.json({ message: "user unauthorized" });
+  }
+
+  next();
+}
+
 const usersController = new UsersController();
 
-userRoutes.post("/", usersController.create);
+userRoutes.post("/", myMiddleware, usersController.create);
 
 module.exports = userRoutes;
