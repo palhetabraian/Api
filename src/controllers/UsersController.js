ec0282c4a67e81f69cacb89f6ce322783b007856
Braian <braiannickolas12@gmail.com>
2025-04-25 00:12:34 -0300

    339
    consertando o codigo

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,27 @@
 const AppError = require("../utils/AppError");
 
+const sqliteConnection = require("../database/sqlite");
+
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    if (!name) {
-      throw new AppError("O nome é obrigatorio");
+    const database = await sqliteConnection();
+    const checkUserExist = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (checkUserExist) {
+      throw new AppErorr("Este e-mail ja esta em uso.");
     }
 
-    response.status(201).json({ name, email, password });
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?,?,?)",
+      [name, email, password]
+    );
+
+    return response.status(201).json();
   }
 }
 /**
