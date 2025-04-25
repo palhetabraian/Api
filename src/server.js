b44fdaf43abb80781f9ec98157f2fb9f1f68827a
Braian <braiannickolas12@gmail.com>
2025-04-24 23:37:55 -0300

    337
    consertando o codigo

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
+
 const express = require("express");
 
 const routes = require("./routes");
