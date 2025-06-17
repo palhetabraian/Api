049e6c72f546e6c5917ca281b415b104901d7f49
Braian <braiannickolas12@gmail.com>
2025-06-17 08:45:37 -0300

    Aula 355
    Exibindo nota

--- src/database/knex/migrations/20250610203514_createNotes.js
+++ src/database/knex/migrations/20250610203514_createNotes.js
@@ -1,7 +1,7 @@
 exports.up = (knex) =>
   knex.schema.createTable("notes", (table) => {
     table.increments("id");
-    table.text("tittle");
+    table.text("title");
     table.text("description");
     table.integer("user_id").references("id").inTable("users");
 
